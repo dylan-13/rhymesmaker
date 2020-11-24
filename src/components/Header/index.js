2f527919d2ae708c361e00bebd2c1ad75ba97c65
Dylan <dylan.berthier13@gmail.com>
2020-11-24 18:14:05 +0100

    third setup commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,11 +3,19 @@ import PropTypes from "prop-types"
 import React from "react"
 import { Image } from "../images"
 
-import { Container, IconImg, TypoImg } from "./Header.module.css"
+import { Container, PageCol, IconImg, TypoImg } from "./Header.module.css"
 
 const Header = ({ siteTitle }) => (
   <header className={Container}>
-    <Image name="iconstars.png" alt="rhymesmaker logo" className={IconImg} />
+    <Link
+      to="/"
+      style={{
+        color: `white`,
+        textDecoration: `none`,
+      }}
+    >
+      <Image name="icon.png" alt="rhymesmaker logo" className={IconImg} />
+    </Link>
     <Link
       to="/"
       style={{
@@ -17,6 +25,10 @@ const Header = ({ siteTitle }) => (
     >
       <Image name="logo.png" alt="rhymesmaker typo logo" className={TypoImg} />
     </Link>
+    <div className={PageCol}>
+      <button>Francais</button>
+      <p />
+    </div>
   </header>
 )
 
